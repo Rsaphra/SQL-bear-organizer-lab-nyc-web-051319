bb72556579b0f4a3782364325ec72451056fef76
Raphael Saphra <rsaphra@gmail.com>
2019-05-21 16:56:21 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 14, "Male", "Black", "Aggressive", true);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Rowdy", 65, "Male", "Black", "Aggressive", true);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Tabitha", 6, "Female", "Black", "Aggressive", true);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 45, "Male", "Brown", "Aggressive", true);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Melissa", 13, "Female", "Black", "Aggressive", true);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Grinch", 16, "Male", "Black", "Aggressive", true);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Wendy", 6, "Female", "Black", "Aggressive", true);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES (null, 15, "Male", "Black", "Aggressive", true);
